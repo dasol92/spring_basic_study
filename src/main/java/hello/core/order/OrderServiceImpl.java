27bab51bdfac626c3fbc8534895717f9b8eb3167
Dasol Kim <zoocanon92@gmail.com>
2022-02-07 20:35:46 +0900

    Singletone: Test duplicated bean; it's called once only

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -20,4 +20,9 @@ public class OrderServiceImpl implements OrderService{
         int discountPrice = discountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    //Singleton 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
