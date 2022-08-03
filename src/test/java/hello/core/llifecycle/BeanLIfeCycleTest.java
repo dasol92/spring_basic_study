b6c47c9384135f566fae06c246000f5dedb6ac82
Dasol Kim <zoocanon92@gmail.com>
2022-08-03 23:56:46 +0900

    Lifecycle: Support callback using @PostConstruct, @PreDestory

--- src/test/java/hello/core/llifecycle/BeanLIfeCycleTest.java
+++ src/test/java/hello/core/llifecycle/BeanLIfeCycleTest.java
@@ -19,7 +19,7 @@ public class BeanLIfeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hollo-spring.dev");
